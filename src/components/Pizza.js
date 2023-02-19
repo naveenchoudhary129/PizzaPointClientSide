b239c3945656a7766802e6abb55f810f1710a37c
Naveen Choudhary <naveenchoudhary129@gmail.com>
2023-02-19 19:39:30 +0530

    second commit

---
+++ src/components/Pizza.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+
+const Pizza = ({pizza}) => {
+    const [quantity , setquantity] = useState(1) ; 
+    const [varient , setvarient] = useState('small') ; 
+  return (
+    <div style={{margin:"50px"}} className="shadow-lg p-3 mb-5 bg-white rounded">
+        <h1>{pizza.name}</h1>
+        <img src={pizza.image} className="img-fluid" style={{height:"200px" , width:"200px"}} />
+
+        <div className='flex-container'>
+
+            <div className='w-100 m-1'>
+                <p>Varients</p>
+                <select className='form-control' value={varient} onChange={(e)=>{setvarient(e.target.value)}}>
+                    {pizza.varients.map(varient=>{
+                        return <option value={varient}>{varient}</option>
+                    })}
+                </select>
+            </div>
+
+            <div className='w-100 m-1'>
+                <p>Quantity</p>
+                <select className='form-control' value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
+                    {[...Array(10).keys()].map((x,i)=>{
+                       return <option value={i+1}>{i+1}</option>
+                    })}
+                </select>
+            </div>
+
+        </div>
+
+        <div className='flex-container'>
+            <div className='m-1 w-100'>
+                <h1 className='mt-2'>Prices: {pizza.prices[0][varient] * quantity} Rs/-</h1>
+            </div>
+
+            <div className='m-1 w-100'>
+                <button className='btn'>ADD TO CART</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Pizza
---
+++ src/pizzadata.js
@@ -0,0 +1,106 @@
+const pizzas = [{
+    "name": "PEPPER BARBECUE CHICKEN",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZwH8lNEU95FnJwWjWUW6Us14naICzKoFne_RubeSAIE2BjaJ8hG7fFcA40ZkMDKSrL4s&usqp=CAU",
+    "description": "Pepper Barbecue Chicken I Cheese"
+}, {
+    "name": "Non Veg Supreme",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://chefchoice.com.np/wp-content/uploads/2020/07/Non-Veg_Supreme.jpg",
+    "description": "Bite into supreme delight of Black Olives, Onions, Grilled Mushrooms, Pepper BBQ Chicken, Peri-Peri Chicken, Grilled Chicken Rashers"
+}, {
+    "name": "GOLDEN CORN Pizza",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/golden_corn_veg.jpg",
+    "description": "Golden Corn - Sweet Corn Pizza"
+}, {
+    "name": "MEXICAN GREEN WAVE",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Mexican_Green_Wave.jpg",
+    "description": "A pizza loaded with crunchy onions, crisp capsicum, juicy tomatoes and jalapeno with a liberal sprinkling of exotic Mexican herbs"
+}, {
+    "name": "DOUBLE CHEESE MARGHERITA",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Double_Cheese_Margherita.jpg",
+    "description": "The ever-popular Margherita - loaded with extra cheese... oodies of it!"
+}, {
+    "name": "VEG LOADED Pizza",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/PrimeLoadedL.jpg",
+    "description": "Tomato | Grilled Mushroom |Jalapeno |Golden Corn | Beans in a fresh pan crust"
+}]
+
+
+export default pizzas ; 
